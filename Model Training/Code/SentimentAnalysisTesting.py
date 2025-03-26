14fbcaad111c61db6dfeadec7ae3a529c4b72316
Toby Deal <sc21td@leeds.ac.uk>
2025-03-26 13:05:42 +0000

    Final settings for running model testing for optimum settings

--- Model Training/Code/SentimentAnalysisTesting.py
+++ Model Training/Code/SentimentAnalysisTesting.py
@@ -8,14 +8,14 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score, f1_score, classification_report
 from sklearn.metrics import confusion_matrix
 from lazypredict.Supervised import LazyClassifier
 
 # Load dataset
 df = pd.read_excel("DatasetTesting.xlsx", sheet_name="Dataset")
 
-# Shuffle and split data into 80/20 split as before
+# Shuffle and split data into 70/30 split as before
 train_df, test_df = train_test_split(df, test_size=0.3, random_state=42, stratify=df["Labelled Rating"])
 
 # Extract text and labels
@@ -64,20 +64,28 @@ X_test_vader = extract_vader_scores(X_test)
 X_train_tfidf, X_test_tfidf, vectoriser = extract_tfidf_features(X_train, X_test)
 X_train_combined = np.hstack((X_train_vader, X_train_tfidf.toarray()))
 X_test_combined = np.hstack((X_test_vader, X_test_tfidf.toarray()))
-# model_combined = LogisticRegression(max_iter=1000)
-# model_combined.fit(X_train_combined, y_train)
-# y_pred_combined = model_combined.predict(X_test_combined)
-# print("Optimum combo with Max features = 500")
-# print(classification_report(y_test, y_pred_combined))
+model = LogisticRegression()
+print("debugging," ,model.max_iter)
+model_combined = LogisticRegression(max_iter=100)
+model_combined.fit(X_train_combined, y_train)
+y_pred_combined = model_combined.predict(X_test_combined)
+print("Optimum combo, 750 max features, default iterations (100)")
+accuracy = accuracy_score(y_test, y_pred_combined)
+print(f"Model Accuracy: {accuracy:.2f}")
+
+# Calculate F1 Score
+f1 = f1_score(y_test, y_pred_combined, average="weighted")
+print(f"F1 Score: {f1:.2f}")
+print(classification_report(y_test, y_pred_combined))
 
 # Test Case 3: Hyperparameter Optimisation (n-grams & stop words handled in extract_tfidf_features function)
 # Already applied bi-grams & stop-word removal above
 
 # Test Case 4: LazyPredict to find the best classifier
-clf = LazyClassifier(verbose=0, ignore_warnings=True, custom_metric=None)
-models, predictions = clf.fit(X_train_combined, X_test_combined, y_train, y_test)
-print("LazyPredict Classifier Results:")
-print(models)
+# clf = LazyClassifier(verbose=0, ignore_warnings=True, custom_metric=None)
+# models, predictions = clf.fit(X_train_combined, X_test_combined, y_train, y_test)
+# print("LazyPredict Classifier Results:")
+# print(models)
 
 # unique_classes = sorted(y_test.unique())
 # plot_confusion_matrix(y_test, y_pred_combined, unique_classes) 
